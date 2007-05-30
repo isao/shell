72cb6534d796f307968ceb6e5783c6dec9cc7ade
Isao Yagi <isao@etherjar.com>
2007-05-30 00:33:04 +0000

    tweaks

--- handy/lssymlink.sh
+++ handy/lssymlink.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-if [ -z $@ ]
-then
-	f='.'
-else
-	f=$@
-fi
-
-ls -lA $f | perl -ne "s|$HOME|~|g; if (m|(\S+ -> \S+)\$|) {print \"\$1\n\";}"
+ls -lA $@ | perl -ne "s|$HOME|~|g; if (m|(\S+ -> \S+)\$|) {print \"\$1\n\";}"
--- handy/print_fromto.pl
+++ handy/print_fromto.pl
@@ -1,8 +1,19 @@
 #!/usr/bin/env perl
 # Usage: print_fromto.pl <string1> <string2> file
+
 my ($from, $to) = (shift, shift);
 while (<>) {
-  $start |= /$from/;
-  print if ($start);
-  exit if /$to/;
+  print if /$from/../$to/;
 }
+
+__END__
+
+#my orig version till i discovered '..'
+while (<>) {
+	$start |= /$from/;
+	print if ($start);
+	exit if /$to/;
+}
+
+#1-liner cli version:
+perl -ne 'print if /from/../to/;' file
