669c121a4a9d4897694f0821661ce33fdf1fa3f8
Isao Yagi <isao@etherjar.com>
2007-06-01 18:07:37 +0000

    coments

--- handy/codegrep.sh
+++ handy/codegrep.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#usage: codgrep.sh [grep param] regex
-#greps certain files from cwd, using supplied regex
-#put optional parameters to grep before regex pattern
+#usage: codgrep.sh [grep param] <pattern>
+#greps certain files from cwd, using supplied <pattern>
+#put optional parameters to grep before <pattern>
 #
 #ie- display files and line with matching pattern
 #  % codegrep.sh <pattern>
