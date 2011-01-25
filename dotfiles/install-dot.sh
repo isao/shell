862addefb9c1c8685c6a34630287cb7ebc5e7541
Isao Yagi <isao@iybetbook.local>
2011-01-24 18:10:34 -0800

    simple install script of dot-*

--- dotfiles/install-dot.sh
+++ dotfiles/install-dot.sh
@@ -1,9 +1,19 @@
 #!/bin/sh -e
 
+if [[ $PWD != $HOME ]]
+then
+	echo "this script should be run from $HOME"
+	exit
+fi
+
 {
-	cd `dirname $0`
+	dotdir=`dirname $0`
+	cd $dotdir
+	echo '- symlink dot-* files to ~'
 	for i in dot-*
 	do
-		echo $i - ${i/dot-/.}
+		echo ln -svi $dotdir/$i ${i/dot-/.}
 	done
+
+	
 }
