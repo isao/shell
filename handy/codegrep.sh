80acdc6fc582734dbfa01d4a3b17e3d6f03bdbe6
Isao Yagi <isao.yagi@gmail.com>
2011-07-15 17:06:46 -0700

    minor usage text edits

--- handy/codegrep.sh
+++ handy/codegrep.sh
@@ -2,15 +2,17 @@
 
 usage()
 {
+  local f=$(basename $0)
 	cat <<MSG >&2
-Usage: `basename $0` [egrep parameters] <pattern>
-Desription: egreps files for <pattern> recursively from cwd, skipping cvs/svn dirs and image files. Tip: put optional parameters to egrep before <pattern>.
-Example: `basename $0` --invert-match 'foo.+bar'
+Usage: $f [egrep parameters] <pattern>
+Desription: egreps files for <pattern> recursively from cwd, skipping cvs/svn
+dirs and image files. Tip: put optional parameters to egrep before <pattern>.
+Example: $f --invert-match 'foo.+bar'
 MSG
 	exit 1
 }
 
-[ -z $1 ] && usage
+[[ -z $1 ]] && usage
 
 find . \( -type d \( -name .svn -or -name CVS \) \) -prune -or \
   -type f -not \( -name '*.gif' -name '*.jpg' -name '*.jpeg' -name '*.png' \) \
--- handy/convert.sh
+++ handy/convert.sh
@@ -7,30 +7,36 @@ preset=Normal
 oldext=.MTS
 newext=.m4v
 dryrun=
+docs=https://trac.handbrake.fr/wiki/CLIGuide
 
 usage() {
   cat <<TXT >&2
 Usage: `basename $0` [options] file1 [file2 [filen..]]
-Pass the file references (or a glob) of video files to convert them to .m4v using
-HandBrakeCLI. Options:
+
+Pass the file references (or a glob) of video files to convert them to .m4v
+using HandBrakeCLI. Options:
+
   -n dry-run (echos commands instead of executing them)
   -o output directory (default is "$outdir")
   -p preset like "Universal" or "Apple TV" (default is "$preset")
   -e new extension (default is "$newext")
   -x old extension (default is "$oldext")
+  -d open docs @ $docs
+
 $1
 TXT
   exit $2
 }
 
 #opt processing
-while getopts 'no:p:e:' opt
+while getopts 'no:p:e:d' opt
 do
   case $opt in
     n ) dryrun='echo';;
     o ) outdir=$OPTARG;;
     p ) preset=$OPTARG;;
     e ) newext=$OPTARG;;
+    d ) open $docs && exit;;
     ? ) usage '' 9;;
   esac
   shift $(($OPTIND-1)); OPTIND=1
