1e25a85e3edf39ae9e788e0a00a54bdc5b7efd50
Isao Yagi <isao.yagi@gmail.com>
2011-08-18 17:40:00 -0700

    - update config - use full path for file or dir - pipe stdin over ssh to bbedit if no args passed

--- bbedit/bbedit.sh
+++ bbedit/bbedit.sh
@@ -1,8 +1,27 @@
-#!/bin/sh
-#  Usage: bbedit.sh file
+#!/bin/sh -x
+# Usage: bbedit.sh [file]
+# have bbedit open a file (or stdin) on your mac by running this script on a
+# remote non-mac. if no arguments, pipe stdin to bbedit.
+
+echo $_
 
 tell=`which yssh ssh | head -1`
+mymac=isao@alloutside-lm.corp
+thisbox=`whoami`@`hostname`
 
-mac=isao@alloutside-lm.corp
-
-$tell $mac bbedit sftp://`whoami`@`hostname`/`pwd`/$1
+if [[ $# -eq 0 ]]
+then
+  $tell $mymac "bbedit --clean --view-top --pipe-title $thisbox:$$" <& 0
+else
+  if [[ -d $1 ]]
+  then
+    ref=$(cd $1 && pwd)
+  elif [[ -f $1 ]]
+  then
+    ref=$(cd `dirname $1` && pwd)/$(basename $1)
+  else
+    echo "neither a dir nor file: $1"
+    exit 1
+  fi
+  $tell $mymac bbedit sftp://$thisbox/$ref
+fi
