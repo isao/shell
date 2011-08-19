0f26524e372e23406bd9eef93310a511169eab83
Isao Yagi <isao.yagi@gmail.com>
2011-08-18 17:52:49 -0700

    minor

--- bbedit/bbedit.sh
+++ bbedit/bbedit.sh
@@ -1,9 +1,7 @@
-#!/bin/sh -x
+#!/bin/sh
 # Usage: bbedit.sh [file]
 # have bbedit open a file (or stdin) on your mac by running this script on a
 # remote non-mac. if no arguments, pipe stdin to bbedit.
-
-echo $_
 
 tell=`which yssh ssh | head -1`
 mymac=isao@alloutside-lm.corp
@@ -11,17 +9,20 @@ thisbox=`whoami`@`hostname`
 
 if [[ $# -eq 0 ]]
 then
-  $tell $mymac "bbedit --clean --view-top --pipe-title $thisbox:$$" <& 0
+  #pipe stdin over ssh to bbedit
+  $tell $mymac "bbedit --clean --view-top --pipe-title $thisbox:$$" <&0
+
 else
   if [[ -d $1 ]]
   then
+    #full path to dir; opening a dir over scp broken in bbedit 10.0.1?    
     ref=$(cd $1 && pwd)
+
   elif [[ -f $1 ]]
   then
+    #full path to file
     ref=$(cd `dirname $1` && pwd)/$(basename $1)
-  else
-    echo "neither a dir nor file: $1"
-    exit 1
   fi
+
   $tell $mymac bbedit sftp://$thisbox/$ref
 fi
