202468ebd41b8fce6f16c5bd6e27ef01c6c52856
Isao Yagi <isao.yagi@gmail.com>
2011-08-23 15:29:32 -0700

    - add totals - chmod + hash bang - formatting - filter input instead of | egrep '^r[1-9]'

--- misc/svn-stats.php
+++ misc/svn-stats.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 /*
   usage: svn log | svn-stats.php [-l]
@@ -5,24 +6,31 @@
   (any command line argument will sort by lines rather than commits)
   sample:
 
-     users  cmt	loc (sorted by commits)
-      isao  481	5118
-    mrsfoo  391	410
-   mrbobby  343	659
+     users  cmt	  loc (sorted by commits)
+    *total  2041	7217
+      isao  481	  5118
+    mrsfoo  391	  410
+   mrbobby  343	  659
    ..etc
 */
 
-function get_stats() {
-  $stats = array();
+function get_stats($file) {
   $first = array('commits' => 1, 'lines' => 1);
+  $stats = array('*sum' => $first);
 
-  foreach(file('php://stdin') as $line) {
+  foreach(file($file) as $line) {
     //r5887 | buildmob | 2011-08-23 13:08:25 -0700 (Tue, 23 Aug 2011) | 4 lines
-    list($r, $u, $d, $n) = explode(' | ', $line);
+
+    if(count(list($r, $u, $d, $n) = explode(' | ', $line)) < 4) {
+    	continue;
+    }
 
     if($stats[$u]) {
       $stats[$u]['commits']++;
       $stats[$u]['lines'] += (int) $n;
+      $stats['*sum']['commits']++;
+      $stats['*sum']['lines'] += (int) $n;
+
     } else {
       $stats[$u] = $first;
     }
@@ -31,7 +39,8 @@ function get_stats() {
 };
 
 function print_stats(array $stats, $sortby) {
-  printf("% 10s  %s\t%s (sorted by %s)\n", 'users', 'cmt', 'loc', $sortby);
+  print "users    commits  commit%  lines    line%  (sorted by $sortby)\n";
+  print "-------  -------  -------  -----  -------\n";
 
   uasort($stats, function($a, $b) use($sortby) {
     return $b[$sortby] - $a[$sortby];
@@ -39,12 +48,15 @@ function print_stats(array $stats, $sortby) {
 
 	foreach($stats as $user => $stat) {
 	  printf(
-	    "% 10s  %d\t%d\n"
-	    , $user, $stat['commits'], $stat['lines']
+	    "% -10s% 6d% 8.1f%%% 7d% 8.1f%%\n"
+	    , $user
+	    , $stat['commits']
+	    , $stat['commits']/$stats['*sum']['commits']*100
+	    , $stat['lines']
+	    , $stat['lines']/$stats['*sum']['lines']*100
 	  );
   }
   print "\n";
 }
 
-$stats = get_stats();
-print_stats($stats, $argc > 1 ? 'lines' : 'commits');
+print_stats(get_stats('php://stdin'), $argc > 1 ? 'lines' : 'commits');
