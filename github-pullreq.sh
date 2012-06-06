738814ae2bbb416775067afef747d19adc434f8c
Isao Yagi <isao.yagi@gmail.com>
2012-06-06 11:06:05 -0700

    script to make pull requests from your current (topic)branch. based on Vladimir Panteleev's https://gist.github.com/1885859

---
+++ github-pullreq.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+# based on Vladimir Panteleev's https://gist.github.com/1885859
+
+die() {
+  echo "$@" >&2
+  exit 1
+}
+
+branch=$(git name-rev --name-only HEAD 2>/dev/null)
+ghusr=$(git config --get github.user)
+
+# pass remote name as arg, or assume it's the first one with your username
+ghrem=${1:-$(git remote -v | grep -Fm1 $ghusr/ | cut -f 1)}
+ghuri=$(git remote -v | grep -m1 github.com: | egrep -o "$ghusr/\w+")
+
+[[ -n $branch ]] || die 'no branch!'
+[[ $branch != master && $branch != develop ]] || die 'use topic branch'
+[[ -n $ghusr ]] || die "github.user not set"
+[[ -n $ghrem ]] || die "no remote found for github.user '$ghusr'"
+
+# push
+git push -v $ghrem $branch
+
+# run this script again to push again, or uncomment git config below to
+# enable this behavior:
+# % git push -v
+# Pushing to git@github.com:isao/mojito.git
+# To git@github.com:isao/mojito.git
+#  = [up to date]      develop -> develop
+#  = [up to date]      master -> master
+#  = [up to date]      mu-ie-splice-bug -> mu-ie-splice-bug
+# updating local tracking ref 'refs/remotes/myfork/develop'
+# updating local tracking ref 'refs/remotes/myfork/master'
+# updating local tracking ref 'refs/remotes/myfork/mu-ie-splice-bug'
+# Everything up-to-date
+#
+#git config branch.$branch.remote $ghrem
+
+# in your browser
+open https://github.com/$ghuri/pull/new/$branch
