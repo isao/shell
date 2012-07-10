b5b7cb57f827e553af0f0a24c5cb52ed7e47eb3a
Isao Yagi <isao.yagi@gmail.com>
2012-07-10 15:20:12 -0700

    ssh tunnel proxy helper

---
+++ proxy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# use this script in ~/.ssh/config instead of 
+#   ProxyCommand  /usr/bin/nc -x 127.0.0.1:9080 %h %p
+
+if nc 127.0.0.1 1080 >/dev/null 2>&1 </dev/null
+then
+	exec nc -x 127.0.0.1:1080 "$@"
+else
+	exec nc "$@" 
+fi
