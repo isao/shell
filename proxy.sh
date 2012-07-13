faef3207c8627679e081e230ec3124a7017d5831
Isao Yagi <isao.yagi@gmail.com>
2012-07-13 10:48:40 -0700

    minor

--- proxy.sh
+++ proxy.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# use this script in ~/.ssh/config instead of 
+# use proxy if available
+# replaces line below in ~/.ssh/config
 #   ProxyCommand  /usr/bin/nc -x 127.0.0.1:9080 %h %p
 
 if nc 127.0.0.1 1080 >/dev/null 2>&1 </dev/null
 then
-	exec nc -x 127.0.0.1:1080 "$@"
+  exec nc -x 127.0.0.1:1080 "$@"
 else
-	exec nc "$@" 
+  exec nc "$@"
 fi
