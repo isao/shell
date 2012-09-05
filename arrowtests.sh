41f0f6fbf77b4fc09ef13b7d8ed5f5f5050ddb0b
Isao Yagi <isao.yagi@gmail.com>
2012-09-05 14:04:55 -0700

    misc

--- arrowtests.sh
+++ arrowtests.sh
@@ -12,7 +12,7 @@ which pgrep arrow java >/dev/null || err 1 'missing prerequisites'
 pgrep -fq arrow_server selenium || err 3 'daemons not running'
 
 logf=/dev/null
-opts="--reuseSession=true --logLevel=ERROR $@"
+opts=" --driver=nodejs --browser=phantomjs --logLevel=ERROR $@"
 
-find . -name '*descriptor.json' | xargs -tn1 arrow $opts \
-    | tee $logf | egrep '^ |..32m[0-9]+'
+find . -name '*descriptor.json' | xargs -tn1 \
+    arrow $opts | tee $logf | egrep '^ |..32m[0-9]+'
