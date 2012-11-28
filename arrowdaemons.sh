0f30198cdc7edcf729fb66f7f2ae3b547cd04432
Isao Yagi <isao.yagi@gmail.com>
2012-11-27 17:04:03 -0800

    trap ^c, etc

--- arrowdaemons.sh
+++ arrowdaemons.sh
@@ -1,16 +1,26 @@
 #!/bin/sh -e
 
 which java pkill arrow_server phantomjs >/dev/null || {
-    echo "error: missing stuff"
+    echo 'error: missing stuff, if arrow not installed globally cd to mojito dir'
     exit $1
 }
 
-pkill -fl arrow_server phantomjs selenium 2>/dev/null && echo "(killed stuff)"
+cleanup() {
+	pkill -fl arrow_ phantomjs selenium- firefox-bin 2>/dev/null \
+	    || echo 'was clean'
+}
 
-echo "* starting arrow_server"
-arrow_server &
-sleep 1
+cleanup
+trap 'cleanup && exit 1' INT
 
-echo "* starting selenium"
+echo '* starting selenium'
+set -x
 jarf=$(brew ls selenium-server-standalone | grep .jar)
 java -Dwebdriver.firefox.profile=default -jar $jarf
+
+
+# echo "* starting arrow_server"
+# arrow_server &
+
+# echo "* starting arrow_selenium"
+# arrow_selenium --open=firefox
