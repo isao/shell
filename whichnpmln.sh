e6e0dce2da392ea115b760e3032f23ed3561439a
Isao Yagi <isao@yahoo-inc.com>
2013-06-25 18:21:26 -0700

    find npm linked package even if bin name different from pkg
    otoh, one could use equiv of `readlink -f` (not avail on stock osx)

--- whichnpmln.sh
+++ whichnpmln.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
-# if an npm module cli is in your path & installed as a symlink (npm ln),
-# print out the associated parent dir. else just act like `which`
+# if an npm module bin is in your path & installed as a symlink (npm ln),
+# print the parent dirname of the source package
+# ... otherwise act like `which`
 
-module=${1:-mojito}
-modpath=$(which $module)
+module=${1:-mojito}         # mojito
+binpath=$(which $module)    # /usr/local/share/npm/bin/mojito
 
-[[ -L $modpath && $modpath =~ $(npm prefix -g) ]] && {
-	cd $(dirname $modpath)
-	[[ -L ../lib/node_modules/$module ]] && {
-		modpath=$(readlink ../lib/node_modules/$module)
-	}
+npmprefix=$(npm prefix -g)  # /usr/local/share/npm
+npmroot=$(npm root -g)/     # /usr/local/share/npm/lib/node_modules
+
+absdir() {
+    cd $(dirname $1) && pwd
 }
 
-echo $modpath
+[[ -L "$binpath" && "$binpath" =~ $npmprefix ]] && {
+    cd $(dirname "$binpath")
+    link0=$(readlink $module)   # ../lib/node_modules/mojito-cli/bin/mojito
+    link1=$(absdir $link0)      # /usr/local/share/npm/lib/node_modules/mojito-cli/bin
+    link2=${link1#$npmroot}     # mojito-cli/bin
+    modname=${link2%%/*}        # mojito-cli
+
+    [[ -L ${npmroot}${modname} ]] && {
+        # /Users/isao/Repos/clistuff/cli
+        binpath=$(readlink ${npmroot}${modname})
+    }
+}
+
+echo $binpath
