c3ce29bf09bb69b72e22c9a22089373ebe007f0a
Isao Yagi <isao@yahoo-inc.com>
2013-07-09 16:29:47 -0700

    rm gui feeback

--- bbedit/Scripts/git-pull.sh
+++ bbedit/Scripts/git-pull.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-dir=$(dirname "$BB_DOC_PATH")
-cd "$dir"
-
-#display message...
-if [[ `$SHELL -c 'which growlnotify'` ]]
-then
-  #...as a growl notification
-  showvia="growlnotify --icon git git-pull $dir"
-else
-  #...in /var/log/system.log; alternatively use 'cat'
-  showvia="logger -t git-pull"
-fi
+cd $(dirname "$BB_DOC_PATH")
 
 #using $SHELL -c '...' so bbedit uses your $PATH and git configs
-$SHELL -c "(git pull --log) | $showvia"
+$SHELL -c "(git pull --log) | logger -t git-pull"
