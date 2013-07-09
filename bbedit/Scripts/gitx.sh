e2012ace71bf11f2729553e3e12afec5a235ace2
Isao Yagi <isao@yahoo-inc.com>
2013-07-09 16:30:12 -0700

    update for gitx v0.14.95

--- bbedit/Scripts/gitx.sh
+++ bbedit/Scripts/gitx.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# using $SHELL to pick up my $PATH
-# you can use git gui or gitk if you prefer
-
-cd `dirname "$BB_DOC_PATH"`
-$SHELL -c gitx
+dir=$(dirname "$BB_DOC_PATH")
+gitdir=$(cd $dir && git rev-parse --show-toplevel)
+/usr/local/bin/gitx --git-dir="$gitdir"
