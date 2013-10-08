9f9ee19a55f2ca082bf51098c9d0da864b500d6e
Isao Yagi <isao@yahoo-inc.com>
2013-10-08 15:40:17 -0700

    rsync first, +comments

--- fswatch.sh
+++ fswatch.sh
@@ -1,8 +1,18 @@
 #!/bin/sh -ex
 which fswatch rsync >/dev/null || usg 1 "missing a prerequisite"
 
-rsync_from=.
-rsync_to="isao@raisegray-vm0.corp.yahoo.com:repos/$(basename $(pwd))/"
-rsync_cmd='rsync -iru --exclude .git --exclude-from=.gitignore --delete'
+rsync_from=$(pwd)/
+rsync_to="isao@raisegray-vm0.corp.yahoo.com:repos/$(basename $rsync_from)"
 
-fswatch . "date +%H:%M:%S && $rsync_cmd $rsync_from $rsync_to"
+# rsync flags:
+# -l aka --links, copy symlinks as symlinks
+# -i aka --itemize-changes, output a change-summary for all updates
+# -r aka --recursive
+# -u skip files that are newer on the receiver
+rsync_cmd="rsync -liru --exclude-from=.gitignore --delete $rsync_from $rsync_to"
+
+#sync now
+$rsync_cmd
+
+#sync on fs changes
+fswatch . "date && $rsync_cmd"
