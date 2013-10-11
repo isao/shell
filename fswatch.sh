6327a4b4e3cef75635e886ddef93c7994ae69ccb
Isao Yagi <isao@yahoo-inc.com>
2013-10-11 14:12:31 -0700

    use longopts, tweak

--- fswatch.sh
+++ fswatch.sh
@@ -4,12 +4,7 @@ which fswatch rsync >/dev/null || usg 1 "missing a prerequisite"
 rsync_from=$(pwd)/
 rsync_to="isao@raisegray-vm0.corp.yahoo.com:repos/$(basename $rsync_from)"
 
-# rsync flags:
-# -l aka --links, copy symlinks as symlinks
-# -i aka --itemize-changes, output a change-summary for all updates
-# -r aka --recursive
-# -u skip files that are newer on the receiver
-rsync_cmd="rsync -liru --exclude-from=.gitignore --delete $rsync_from $rsync_to"
+rsync_cmd="rsync --recursive --links --safe-links --update --delete --itemize-changes --exclude-from=.gitignore $rsync_from $rsync_to"
 
 #sync now
 $rsync_cmd
