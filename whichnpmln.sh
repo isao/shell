366d3584c101842197cb4ed6b97a04d74581e08e
Isao Yagi <isao@yahoo-inc.com>
2013-11-26 16:21:14 -0800

    quote in case of paths containing spaces

--- whichnpmln.sh
+++ whichnpmln.sh
@@ -5,23 +5,23 @@
 # ... otherwise act like `which`
 
 module=${1:-mojito}         # mojito
-binpath=$(which $module)    # /usr/local/share/npm/bin/mojito
+binpath=$(which "$module")    # /usr/local/share/npm/bin/mojito
 
 npmprefix=$(npm prefix -g)  # /usr/local/share/npm
 npmroot=$(npm root -g)/     # /usr/local/share/npm/lib/node_modules
 
 absdir() {
-    cd $(dirname $1) && pwd
+    cd $(dirname "$1") && pwd
 }
 
 [[ -L "$binpath" && "$binpath" =~ $npmprefix ]] && {
     cd $(dirname "$binpath")
-    link0=$(readlink $module)   # ../lib/node_modules/mojito-cli/bin/mojito
-    link1=$(absdir $link0)      # /usr/local/share/npm/lib/node_modules/mojito-cli/bin
+    link0=$(readlink "$module") # ../lib/node_modules/mojito-cli/bin/mojito
+    link1=$(absdir "$link0")    # /usr/local/share/npm/lib/node_modules/mojito-cli/bin
     link2=${link1#$npmroot}     # mojito-cli/bin
     modname=${link2%%/*}        # mojito-cli
 
-    [[ -L ${npmroot}${modname} ]] && {
+    [[ -L "${npmroot}${modname}" ]] && {
         # /Users/isao/Repos/clistuff/cli
         binpath=$(readlink ${npmroot}${modname})
     }
