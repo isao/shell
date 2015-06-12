95d95b7d3dda3e23cfbe3a8c536ccc4734a64678
Isao Yagi <isao.yagi@gmail.com>
2015-06-12 12:26:15 -0700

    add osx notification, move to background

--- ctag-reach.sh
+++ ctag-reach.sh
@@ -1,12 +1,24 @@
 #!/bin/sh -e
 
+makeTags()
+{
+    /usr/local/bin/ctags \
+        --languages=typescript \
+        --exclude=OPEN_SOURCE_SOFTWARE \
+        --exclude=node_modules \
+        --exclude=napa \
+        --excmd=number \
+        --tag-relative=no \
+        --fields=+a+m+n+S \
+        --recurse
+}
+
+alert()
+{
+    osascript -e 'display notification "Tags generated for ~/work/MRGIT/ReachClient" with title "ctags complete."'
+}
+
+
 cd ~/work/MRGIT/ReachClient/Web/scripts
-ctags \
-    --languages=typescript \
-    --exclude=OPEN_SOURCE_SOFTWARE \
-    --exclude=node_modules \
-    --exclude=napa \
-    --excmd=number \
-    --tag-relative=no \
-    --fields=+a+m+n+S \
-    --recurse
+
+(makeTags && alert) &
