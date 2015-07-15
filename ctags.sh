91f019d2ae5de33a74fbbc8011ba91dbd270fd43
Isao Yagi <isao.yagi@gmail.com>
2015-07-15 15:34:42 -0700

    Add comments, better check for MRGIT

--- ctags.sh
+++ ctags.sh
@@ -1,35 +1,37 @@
 #!/bin/sh -e
 
-mrgit()
-{
-    if [[ $dir =~ /MRGIT/[^/]+ ]]
-    then
-        dir="$dir/Web/scripts"
-        language='--languages=typescript'
-        excludes='--exclude=OPEN_SOURCE_SOFTWARE --exclude=node_modules --exclude=napa'
-    fi
-}
+# Wrapper script to do BBEdit compatible ctags, optionally when invoked from a
+# BBEdit Script Menu.
 
-makeTags()
+maketags()
 {
     cd "$dir"
-    ctags $language $excludes $ctagflags
+    logger "ctags $language $excludes $ctagflags $@"
+    ctags $language $excludes $ctagflags $@
 }
 
 alert()
 {
-    osascript -e "display notification \"$dir\" with title \"ctags: $scriptname\""
+    osascript -e "display notification \"$dir\" with title \"ctags: $@\""
 }
 
-
+# Handle invocation from BBEdit Script Menu
 [[ -n $BB_DOC_PATH ]] && cd "$(dirname "$BB_DOC_PATH")"
 
+# Always
 ctagflags='--excmd=number --tag-relative=no --fields=+a+m+n+S --recurse'
+
+# Defaults
 language=''
-excludes=''
-scriptname="$(basename "$0")"
+excludes='--exclude=node_modules --exclude=.git'
 dir="$(git rev-parse --show-toplevel 2>/dev/null)"
 
-mrgit
+# special case for MRGIT
+[[ $(git remote -v 2>/dev/null) =~ /MRGIT/_git/ReachClient ]] && {
+    dir="$dir/Web/scripts"
+    language='--languages=typescript'
+    excludes="$excludes --exclude=OPEN_SOURCE_SOFTWARE --exclude=napa"
+}
 
-(makeTags && alert) &
+# main
+(maketags $@ && alert $(basename "$0")) &
