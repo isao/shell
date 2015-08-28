e4afa84ca2e022c4f88458cd4354c3bb1be0c2e0
Isao Yagi <isao.yagi@gmail.com>
2015-08-28 16:25:52 -0700

    Add ts dir to ctags.sh

--- ctags.sh
+++ ctags.sh
@@ -7,14 +7,14 @@
 
 maketags()
 {
-    cd "$dir"
-    logger "ctags $language $excludes $ctagflags $*"
-    ctags $language $excludes $ctagflags $@
+    cd "$base"
+    #logger "ctags $language $excludes $ctagflags $*"
+    ctags $language $excludes $ctagflags $@ $paths
 }
 
 alert()
 {
-    osascript -e "display notification \"$dir\" with title \"ctags: $1\""
+    osascript -e "display notification \"$base\" with title \"ctags: $1\""
 }
 
 # Handle invocation from BBEdit Script Menu
@@ -26,11 +26,12 @@ ctagflags='--excmd=number --tag-relative=no --fields=+a+m+n+S --recurse'
 # Defaults
 language=''
 excludes='--exclude=node_modules --exclude=.git'
-dir="$(git rev-parse --show-toplevel 2>/dev/null)"
+base="$(git rev-parse --show-toplevel 2>/dev/null)"
+paths=
 
 # special case for MRGIT
 [[ $(git remote -v 2>/dev/null) =~ /MRGIT/_git/ReachClient ]] && {
-    dir="$dir/Web/scripts"
+    paths='Web/scripts Web/typescript'
     language='--languages=typescript'
     excludes="$excludes --exclude=OPEN_SOURCE_SOFTWARE --exclude=napa"
 }
