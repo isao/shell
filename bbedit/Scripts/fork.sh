276ef71d335c6c150970152d7ddbcb4aadd2a90d
Isao Yagi <isao.yagi@gmail.com>
2020-03-04 13:08:30 -0800

    fix "open in fork" script

--- bbedit/Scripts/fork.sh
+++ bbedit/Scripts/fork.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
+
 cd "$(dirname "$BB_DOC_PATH")"
-gitdir=$(git rev-parse --show-toplevel 2>/dev/null)
-/usr/local/bin/fork --git-dir $dir
+
+/usr/local/bin/fork -C "$(git rev-parse --show-toplevel 2>/dev/null)"
