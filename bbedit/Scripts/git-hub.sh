eaa6816b9ad5580fa0b6b9a28ffdbedc39fb232b
Isao Yagi <isao.yagi@gmail.com>
2023-01-30 15:23:44 -0800

    git-hub: include selected lines

--- bbedit/Scripts/git-hub.sh
+++ bbedit/Scripts/git-hub.sh
@@ -1,3 +1,14 @@
-#!/bin/sh -e
+#!/bin/bash -e
 cd "$(dirname "$BB_DOC_PATH")"
-git-hub "$BB_DOC_PATH"
+
+line=""
+if [[ $BB_DOC_SELSTART_COLUMN -ne $BB_DOC_SELEND_COLUMN || $BB_DOC_SELSTART_LINE -ne $BB_DOC_SELEND_LINE ]]
+then
+    line="#L$BB_DOC_SELSTART_LINE"
+    if [[ $BB_DOC_SELSTART_LINE -ne $BB_DOC_SELEND_LINE ]]
+    then
+        line="$line-L$BB_DOC_SELEND_LINE"
+    fi
+fi
+
+git-hub "$BB_DOC_PATH" "$line"
