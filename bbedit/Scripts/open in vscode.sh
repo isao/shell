e857954272dceeb1924abad2230d7e647c487925
Isao Yagi <isao.yagi@gmail.com>
2024-08-20 11:28:03 -0700

    Open in VS Code

--- bbedit/Scripts/open in vscode.sh
+++ bbedit/Scripts/open in vscode.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-project_dir=$(pkg-root "$(dirname "$BB_DOC_PATH")")
+# project_dir=$(pkg-root "$(dirname "$BB_DOC_PATH")")
+cd "$(realpath "$(dirname "$BB_DOC_PATH")")"
+project_dir="$(git rev-parse --show-toplevel)"
 
 file_ref="$BB_DOC_PATH:$BB_DOC_SELSTART_LINE:$BB_DOC_SELEND_COLUMN"
 
