48a3404cdde5b73b822144c9815d53436cc41301
Isao Yagi <isao.yagi@gmail.com>
2025-09-16 16:36:15 -0700

    minor

--- bbedit/Scripts/fork.sh
+++ bbedit/Scripts/fork.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
-cd "$(realpath "$(dirname "$BB_DOC_PATH")")"
+#cd "$(realpath "$(dirname "$BB_DOC_PATH")")"
+#
+#fork status -C "$(git rev-parse --show-toplevel 2>/dev/null)" > /dev/null
 
-fork status -C "$(git rev-parse --show-toplevel 2>/dev/null)" > /dev/null
+fork status -C "$(dirname "$BB_DOC_PATH")"
--- bbedit/Scripts/open in vscode.sh
+++ bbedit/Scripts/open in vscode.sh
@@ -2,8 +2,9 @@
 
 # project_dir=$(pkg-root "$(dirname "$BB_DOC_PATH")")
 cd "$(realpath "$(dirname "$BB_DOC_PATH")")"
+
 project_dir="$(git rev-parse --show-toplevel)"
 
 file_ref="$BB_DOC_PATH:$BB_DOC_SELSTART_LINE:$BB_DOC_SELEND_COLUMN"
 
-code --goto "$file_ref" "$project_dir" > /dev/null
+code "$project_dir" --goto "$file_ref" >/dev/null
